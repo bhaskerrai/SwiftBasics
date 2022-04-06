e2f0c614f52d22a56aa72f6028d5b4d0d5618dfb
bhaskerrai <brai285@gmail.com>
2022-04-06 14:37:46 +0500

    added type annotations
    added type annotations to the file of basics

--- SwiftBasics.playground/Contents.swift
+++ SwiftBasics.playground/Contents.swift
@@ -1,3 +1,20 @@
 import UIKit
 
 var greeting = "Hello, playground"
+
+//Type annotations:
+var abc : String
+abc = "hey motherfucker"
+print(abc)
+
+//“Constant and variable names can contain almost any character, including Unicode characters:”
+
+let 🥲 = "fucked up!"
+print(🥲)
+
+//single line comment
+/*
+ multip line comment
+ */
+
+
